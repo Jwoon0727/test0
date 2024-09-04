7bd8cbb938a80e96fa163f4e18c1c9f930cc34cd
Jwoon0727 <149401369+Jwoon0727@users.noreply.github.com>
2024-09-04 16:04:20 +0900

    커밋 메시지

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from 'next-pwa';
+import dotenv from 'dotenv';
 
-export default nextConfig;
+// Load environment variables from .env file
+dotenv.config();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,      // Enable React strict mode for improved error handling
+    swcMinify: true,            // Enable SWC minification for improved performance
+    compiler: {
+        removeConsole: process.env.NODE_ENV !== "development" // Remove console.log in production
+    },
+    basePath: process.env.BASE_PATH || '/test0', // Set basePath based on environment variable or default to '/test0'
+    assetPrefix: process.env.BASE_PATH || '/test0', // Ensure asset paths are correct
+    output: 'export',           // Add this line to enable static export
+};
+
+export default withPWA({
+    dest: "public",         // destination directory for the PWA files
+    disable: process.env.NODE_ENV === "development", // disable PWA in the development environment
+    register: true,         // register the PWA service worker
+    skipWaiting: true,      // skip waiting for service worker activation
+})(nextConfig);
